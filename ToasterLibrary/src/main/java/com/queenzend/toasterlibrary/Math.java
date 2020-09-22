5a0a739198935861ad7e0de2d0ce108d33e87da6
Navin <queenzend@Queenzends-MacBook-Air.local>
2020-09-22 17:41:28 +0530

    wscallfunction2

---
+++ ToasterLibrary/src/main/java/com/queenzend/toasterlibrary/Math.java
@@ -0,0 +1,13 @@
+package com.queenzend.toasterlibrary;
+
+public class Math {
+
+    static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        int result = sum(2,3);
+        System.out.println("Sum of numbers is: " + result);
+    }
+}
--- ToasterLibrary/src/main/java/com/queenzend/toasterlibrary/SaveDataAll.java
+++ ToasterLibrary/src/main/java/com/queenzend/toasterlibrary/SaveDataAll.java
@@ -17,8 +17,16 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class SaveDataAll {
-    public static void CallServer(final Context c, String email) {
-//        Toast.makeText(c, "token" + "jayuuuuu", Toast.LENGTH_SHORT).show();
+    static int CallServer(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        int result = CallServer(2,3);
+        System.out.println("Sum of numbers is: " + result);
+    }
+
+    public static void CallServer11(final Context c, String email) {
         Toast.makeText(c, "welcome"+"-----"+email, Toast.LENGTH_SHORT).show();
         class UserLogin extends AsyncTask<String, Void, String> {
             String loginUrl = "http://k2key.in/marketing_plateform_CI/UserController/saveDeviceInfo";
