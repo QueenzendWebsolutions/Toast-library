8c3d047ee138582ee842428b94effe656460b518
Navin <queenzend@Queenzends-MacBook-Air.local>
2020-09-22 13:32:35 +0530

    wscode

--- app/src/main/java/com/queenzend/toasterexample/MainActivity.java
+++ app/src/main/java/com/queenzend/toasterexample/MainActivity.java
@@ -1,10 +1,8 @@
 package com.queenzend.toasterexample;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-import com.queenzend.toasterlibrary.ToasterMessage;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
