06c9bf17459604b513fe0847de5abb0c033c0a9a
Navin <queenzend@Queenzends-MacBook-Air.local>
2020-10-02 17:41:31 +0530

    Save registered user

---
+++ ToasterLibrary/src/main/java/com/queenzend/toasterlibrary/URLUtils.java
@@ -0,0 +1,6 @@
+package com.queenzend.toasterlibrary;
+
+public class URLUtils {
+    public static String Url_Login = "http://k2key.in/marketing_plateform_CI/UserController/saveDeviceInfo";
+    public static String Url_appUserRegistration=  "http://k2key.in/marketing_plateform_CI/Webservice/appUserRegistration";
+}
